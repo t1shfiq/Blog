77199ae20770fdd791eed34501cf82d82c7c9310
t1shfiq <ali.tashfiq4@gmail.com>
2020-12-22 20:59:56 -0500

    Creating API Endpoint to get an individual blog post

--- blog-api/api/models/posts.js
+++ blog-api/api/models/posts.js
@@ -3,29 +3,35 @@ const fs = require('fs');
 
 class Post {
 
-    
-
     get(){
         return this.readData();
     }
 
-    getIndividualBlog(){
-
+    getIndividualBlog(postId){
+        const post = this.readData();
+        const foundPost = post.find((post) => post.id == postId);
+        return foundPost;
     }
 
-    add(){
-
-
+    add(newPost) { 
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(currentPosts);
     }
 
     readData(){
-        
         // JSON.parse takes JSON data as input and returns a new JavaScript object. 
         // Otherwise, we would just have a string of data with properties we can’t access.
         let rawdata = fs.readFileSync(PATH);
         let posts = JSON.parse(rawdata);
         return posts;
+    }
 
+    storeData(rawData){
+
+        // object to JSON
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 
 }
--- blog-api/app.js
+++ blog-api/app.js
@@ -3,15 +3,24 @@ const app = express ();
 const Post = require("./api/models/posts");
 const postsData = new Post();
 
-app.get("/", (req,res) => {
+app.get("/api/posts", (req,res) => {
 
+    postsData.add(test);
     res.status(200).send(postsData.get());
-
 })
+
+app.get("/api/posts/:id", (req,res) => {
+    const postId = req.params.id;
+    const foundPost = postsData.getIndividualBlog(postId);
+
+    if(foundPost) {
+        res.status(200).send(foundPost)
+    } else {
+        res.status(404).send("Not Found");
+    }
+});
 
 // Initialize the server
 app.listen(3000, () => {
-    
     console.log ("Listening on localhost 3000")
-
 })
